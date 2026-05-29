705f42536c8c0df06253dd441f13eac499dcbdf0
Ilya Shirokov <133996191+Elias-Wide@users.noreply.github.com>
2026-05-29 14:57:46 +0300

    Update departments.py

--- src/repositories/departments.py
+++ src/repositories/departments.py
@@ -1,4 +1,12 @@
 from src.repositories.base import SQLAlchemyRepository
 
 
-class DepartmentsRepo(SQLAlchemyRepository): ...
+class DepartmentsRepo(SQLAlchemyRepository):
+    """
+    Repository for managing Department records.
+
+    Inherits core CRUD operations and implements custom recursive
+    Common Table Expressions (CTE) for tree traversal.
+    """
+
+    model = DepartmentsORM
