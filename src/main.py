7b1d5538897ad004439a608cca9e407e0ec48f49
Ilya Shirokov <ilshi2904@gmail.com>
2026-05-29 22:24:00 +0300

    create main

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from src.api.endpoints.v1.routers import api_router_v1 as main_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(main_router)
